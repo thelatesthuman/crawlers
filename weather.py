995fe3679f49067777423fc4c332b0f9a80e288b
thelatesthuman <jporterqoop@gmail.com>
2023-03-19 11:47:55 -0500

    Added formating to output of weather.py

--- weather.py
+++ weather.py
@@ -4,18 +4,20 @@ from bs4 import BeautifulSoup
 
 try:
     # Send an HTTP request to weather.com url
-    url = str('your weather.com url here')
+    url = 'your weather.com url here'
     response = requests.get(url)
 
     # Parse file and extract the temp, weather, and day/night temps
     soup = BeautifulSoup(response.content, 'html.parser')
+    location = soup.find('h1', class_='CurrentConditions--location--1YWj_').text.strip()
     current_temp = soup.find('span', class_='CurrentConditions--tempValue--MHmYY').text.strip()
     current_weather = soup.find('div', class_='CurrentConditions--phraseValue--mZC_p').text.strip()
     daynight = soup.find('div', class_='CurrentConditions--tempHiLoValue--3T1DG').text.strip() 
     
     # print parsed data to terminal
-    print(current_temp)
-    print(current_weather)
+    print('\nWeather in {}\n------------------------'.format(location))
+    print('Current Temp: {}'.format(current_temp))
+    print('Current Weather: {}'.format(current_weather))
     print(daynight)
 except Exception as e:
     print("An error has occured", e)
