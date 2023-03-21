d5d94117bcfc6ea2a084f75732b9b1f1351f3033
thelatesthuman <jporterqoop@gmail.com>
2023-03-20 21:38:51 -0500

    Added enumerate to quotes_xml.py

--- quotes_xml.py
+++ quotes_xml.py
@@ -14,11 +14,10 @@ page_count = 1
 while url:
     # Use requests to get the webpage
     response = requests.get(url)
-    # Debugging tool
-    print(response.status_code)
     
     # Initialize BeautifulSoup by creating a soup object
     soup = BeautifulSoup(response.content, 'html.parser')
+    
     # Parse the web page for all elements that are a division with the class "quote"
     quotes_class = soup.find_all('div', class_='quote')
 
@@ -26,8 +25,7 @@ while url:
     page = ET.SubElement(root, "Page", number=str(page_count))
 
     # Parse through each element found and extract the quote, author, and tags
-    quote_count = 1
-    for quote_class in quotes_class:
+    for quote_count,quote_class in enumerate((quotes_class), start=1):
         # Extract quotes, authors, and tags from quote_class
         quote = quote_class.find('span', class_='text').text.strip()
         author = quote_class.find('small', class_='author').text.strip()
@@ -48,20 +46,17 @@ while url:
         ET.SubElement(quote_element, "Author").text = author
         ET.SubElement(quote_element, "Tags").text = tags
         
-        tag_count = 1
-        for tag in tags.split('\n'):
+        for tag_count,tag in enumerate(tags.split('\n'), start=1):
             words = tag.split(',')
             for word in words:
                 ET.SubElement(tag_element, "Tag", number=str(tag_count)).text = word.strip()
-                tag_count += 1
-
-        quote_count += 1
-    
+ 
+    print("Page {} complete!".format(str(page_count)))
+    page_count +=1
     # Check to see if there is a next page
     try:
         next_link = soup.find('li', class_='next').find('a')
         url = "https://quotes.toscrape.com" + next_link['href']
-        page_count +=1
     except AttributeError:
         print("No more pages to parse")
         url = None
