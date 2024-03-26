939af878e820b4ebb25a88a2917f50ae5ff58b92
thelatesthuman <jporterqoop@gmail.com>
2024-03-25 21:14:53 -0500

    adding phoneAndEmail.py

---
+++ phoneAndEmail.py
@@ -0,0 +1,44 @@
+#!python3
+
+#Finds phone numbers and emails on webpage
+import re
+import requests
+import pyinputplus as pyip
+
+#Phone number Regex
+phoneRegex = re.compile(r'''(
+                        (\d{3}|\(\d{3}\))?              #Area code
+                        (\s|\.|-)?                      #Separator
+                        (\d{3})                         #First 3 digits
+                        (\s|\.|-)                       #Separator
+                        (\d{4})                         #Last 4 digits
+                        (\s*|ext|ext.|x)\s*(\d{2,5})?   #Extension
+                        )''', re.VERBOSE)
+
+#Email Regex
+emailRegex = re.compile(r'''(
+                        [a-zA-Z0-9._%+-]+               #Username
+                        @                               #@ symbol
+                        [a-zA-Z0-9._%+-]+               #Domain
+                        (\.[a-zA-Z]{2,4})               #Dot Something
+                        )''',re.VERBOSE)
+#Request webpage
+webpage = requests.get(pyip.inputStr("What webpage you want dawg? "))
+
+#Find matches in webpage
+text = webpage.text
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1],groups[3],groups[5]])
+    if groups[7] != '':
+        phoneNum += ' x' + groups[7]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+#Copy results to the clipboard
+if len(matches) > 0:
+    print('Matches found: ')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or emails found.')
